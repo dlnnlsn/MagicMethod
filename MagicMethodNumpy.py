558afc2a4ff7cfa64e8f6c8ace1283c7f94456b7
Dylan Nelson <nlsdyl001@myuct.ac.za>
2021-01-09 01:43:39 +0200

    weight matrix and actual scores are often multiplied, so store the product

--- MagicMethodNumpy.py
+++ MagicMethodNumpy.py
@@ -7,15 +7,21 @@ def badness(difficulties, scores, test_weights, actual_scores):
     )
 
 
-def scores_from_difficulties(difficulties, weight_matrix, actual_scores):
-    numerators = np.nansum(difficulties * weight_matrix * actual_scores, axis=1)
+def scores_from_difficulties(
+    difficulties, weight_matrix, actual_scores, weight_matrix_times_actual_scores
+):
+    numerators = np.sum(difficulties * weight_matrix_times_actual_scores, axis=1)
     denominators = np.sum(weight_matrix * difficulties ** 2, axis=1)
     return numerators / denominators
 
 
-def difficulties_from_scores(scores, weight_matrix, actual_scores):
+def difficulties_from_scores(
+    scores, weight_matrix, actual_scores, weight_matrix_times_actual_scores
+):
     denominators = np.sum(scores[np.newaxis].T ** 2 * weight_matrix, axis=0)
-    numerators = np.nansum(scores[np.newaxis].T * actual_scores * weight_matrix, axis=0)
+    numerators = np.sum(
+        scores[np.newaxis].T * weight_matrix_times_actual_scores, axis=0
+    )
     lambda_coeff = np.sum(1 / denominators)
     lambda_value = (1 - numerators.dot(denominators)) / lambda_coeff
     return (lambda_value + numerators) / denominators
@@ -25,13 +31,23 @@ def calculate_scores_and_difficulties(
     test_weights, actual_scores, tolerance=1e-3, max_iterations=100
 ):
     weight_matrix = ~np.isnan(actual_scores) * test_weights
+    weight_matrix_times_actual_scores = weight_matrix * np.nan_to_num(actual_scores)
     _, tests = actual_scores.shape
     difficulties = np.ones(tests) / tests
-    scores = scores_from_difficulties(difficulties, weight_matrix, actual_scores)
+    scores = scores_from_difficulties(
+        difficulties, weight_matrix, actual_scores, weight_matrix_times_actual_scores
+    )
     previous_badness = badness(difficulties, scores, test_weights, actual_scores)
     for _ in range(max_iterations):
-        difficulties = difficulties_from_scores(scores, weight_matrix, actual_scores)
-        scores = scores_from_difficulties(difficulties, weight_matrix, actual_scores)
+        difficulties = difficulties_from_scores(
+            scores, weight_matrix, actual_scores, weight_matrix_times_actual_scores
+        )
+        scores = scores_from_difficulties(
+            difficulties,
+            weight_matrix,
+            actual_scores,
+            weight_matrix_times_actual_scores,
+        )
         new_badness = badness(difficulties, scores, test_weights, actual_scores)
         if abs(new_badness - previous_badness) < tolerance:
             return difficulties, scores
