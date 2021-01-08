83d1ed1bcded3d38f7607a23467b61161ad5ede9
Dylan Nelson <nlsdyl001@myuct.ac.za>
2021-01-09 01:35:59 +0200

    score_mask only ever appears next to test_weights, so combine them into a single parameter

--- MagicMethodNumpy.py
+++ MagicMethodNumpy.py
@@ -7,15 +7,15 @@ def badness(difficulties, scores, test_weights, actual_scores):
     )
 
 
-def scores_from_difficulties(difficulties, test_weights, actual_scores, score_mask):
-    numerators = np.nansum(difficulties * test_weights * actual_scores, axis=1)
-    denominators = np.sum(test_weights * difficulties ** 2 * score_mask, axis=1)
+def scores_from_difficulties(difficulties, weight_matrix, actual_scores):
+    numerators = np.nansum(difficulties * weight_matrix * actual_scores, axis=1)
+    denominators = np.sum(weight_matrix * difficulties ** 2, axis=1)
     return numerators / denominators
 
 
-def difficulties_from_scores(scores, test_weights, actual_scores, score_mask):
-    denominators = np.sum(scores[np.newaxis].T ** 2 * score_mask * test_weights, axis=0)
-    numerators = np.nansum(scores[np.newaxis].T * actual_scores * test_weights, axis=0)
+def difficulties_from_scores(scores, weight_matrix, actual_scores):
+    denominators = np.sum(scores[np.newaxis].T ** 2 * weight_matrix, axis=0)
+    numerators = np.nansum(scores[np.newaxis].T * actual_scores * weight_matrix, axis=0)
     lambda_coeff = np.sum(1 / denominators)
     lambda_value = (1 - numerators.dot(denominators)) / lambda_coeff
     return (lambda_value + numerators) / denominators
@@ -24,20 +24,14 @@ def difficulties_from_scores(scores, test_weights, actual_scores, score_mask):
 def calculate_scores_and_difficulties(
     test_weights, actual_scores, tolerance=1e-3, max_iterations=100
 ):
-    score_mask = ~np.isnan(actual_scores)
+    weight_matrix = ~np.isnan(actual_scores) * test_weights
     _, tests = actual_scores.shape
     difficulties = np.ones(tests) / tests
-    scores = scores_from_difficulties(
-        difficulties, test_weights, actual_scores, score_mask
-    )
+    scores = scores_from_difficulties(difficulties, weight_matrix, actual_scores)
     previous_badness = badness(difficulties, scores, test_weights, actual_scores)
     for _ in range(max_iterations):
-        difficulties = difficulties_from_scores(
-            scores, test_weights, actual_scores, score_mask
-        )
-        scores = scores_from_difficulties(
-            difficulties, test_weights, actual_scores, score_mask
-        )
+        difficulties = difficulties_from_scores(scores, weight_matrix, actual_scores)
+        scores = scores_from_difficulties(difficulties, weight_matrix, actual_scores)
         new_badness = badness(difficulties, scores, test_weights, actual_scores)
         if abs(new_badness - previous_badness) < tolerance:
             return difficulties, scores
