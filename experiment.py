3df639826255258258100a34fb6ae7d07c5bdb8d
Dylan Nelson <nlsdyl001@myuct.ac.za>
2021-01-24 04:24:12 +0200

    reformat code

--- experiment.py
+++ experiment.py
@@ -4,71 +4,117 @@ import scipy.special
 import scipy.optimize
 from functools import partial
 
-def objective(f,actual_scores,test_weights):
+
+def objective(f, actual_scores, test_weights):
     students = len(actual_scores)
     tests = len(test_weights)
+
     def obj(scores_and_difficulties):
         scores = scores_and_difficulties[:students]
         difficulties = scores_and_difficulties[students:]
         total = 0
         for i in range(students):
             for j in range(tests):
-                if actual_scores[i][j] is not None and not np.isnan(actual_scores[i][j]):
-                    total += test_weights[j] * (f(scores[i],difficulties[j]) - actual_scores[i][j])**2
+                if actual_scores[i][j] is not None and not np.isnan(
+                    actual_scores[i][j]
+                ):
+                    total += (
+                        test_weights[j]
+                        * (f(scores[i], difficulties[j]) - actual_scores[i][j]) ** 2
+                    )
         return total
+
     return obj
 
-def initial(students,tests):
-    return np.array([1] * students + [1/tests] * tests)
 
-def normalise_difficulties(students,scores_and_difficulties):
+def initial(students, tests):
+    return np.array([1] * students + [1 / tests] * tests)
+
+
+def normalise_difficulties(students, scores_and_difficulties):
     return np.sum(scores_and_difficulties[students:]) - 1
 
-def get_bounds(students,tests):
-    return [(0,None)] * students + [(0,1)] * tests
 
-def minimize(f,actual_scores,test_weights,**kwargs):
+def get_bounds(students, tests):
+    return [(0, None)] * students + [(0, 1)] * tests
+
+
+def minimize(f, actual_scores, test_weights, **kwargs):
     students = len(actual_scores)
     tests = len(test_weights)
-    x0 = initial(students,tests)
-    bounds = get_bounds(students,tests)
-    difficulty_constraint = partial(normalise_difficulties,students)
-    constraints = [{"type": "eq","fun": difficulty_constraint}]
-    return scipy.optimize.minimize(objective(f,actual_scores,test_weights),x0,bounds=bounds,constraints=constraints,**kwargs)
+    x0 = initial(students, tests)
+    bounds = get_bounds(students, tests)
+    difficulty_constraint = partial(normalise_difficulties, students)
+    constraints = [{"type": "eq", "fun": difficulty_constraint}]
+    return scipy.optimize.minimize(
+        objective(f, actual_scores, test_weights),
+        x0,
+        bounds=bounds,
+        constraints=constraints,
+        **kwargs
+    )
 
-relu = lambda k: lambda s,d: 1 - np.log(1 + np.exp(-k * d * s)) / np.log(2)
+
+relu = lambda k: lambda s, d: 1 - np.log(1 + np.exp(-k * d * s)) / np.log(2)
 
 candidate_functions = {
-    "Logistic Function" : lambda s,d: 2/(1 + np.exp(-d * s)) - 1,
-    "Error Function": lambda s,d: scipy.special.erf(d * s),
-    "Hyperbolic Tangent": lambda s,d: np.tanh(d * s),
-    "Gudermannian": lambda s,d: 4 * np.arctan(np.tanh(d * s / 2)) / np.pi,
-    "Normal Product": lambda s,d: d * s,
-    "Arctan": lambda s,d: 2 * np.arctan(d * s) / np.pi,
-    "Cutoff Product": lambda s,d: min(d * s,1),
-    "x/sqrt(1 + x^2)": lambda s,d: s * d / np.sqrt(1 + (d * s)**2),
-    "x/(1 + x)": lambda s,d: s * d / (1 + s * d),
+    "Logistic Function": lambda s, d: 2 / (1 + np.exp(-d * s)) - 1,
+    "Error Function": lambda s, d: scipy.special.erf(d * s),
+    "Hyperbolic Tangent": lambda s, d: np.tanh(d * s),
+    "Gudermannian": lambda s, d: 4 * np.arctan(np.tanh(d * s / 2)) / np.pi,
+    "Normal Product": lambda s, d: d * s,
+    "Arctan": lambda s, d: 2 * np.arctan(d * s) / np.pi,
+    "Cutoff Product": lambda s, d: min(d * s, 1),
+    "x/sqrt(1 + x^2)": lambda s, d: s * d / np.sqrt(1 + (d * s) ** 2),
+    "x/(1 + x)": lambda s, d: s * d / (1 + s * d),
     "RELU(1)": relu(1),
     "RELU(2)": relu(2),
     "RELU(4)": relu(4),
     "RELU(8)": relu(8),
     "RELU(16)": relu(16),
-    "Smoothstep 3": lambda s,d: (lambda x: 0 if x <= 0 else 3*x**2 - 2*x**3 if x <= 1 else 1)(s * d),
-    "Smoothstep 5": lambda s,d: (lambda x: 0 if x <= 0 else 6*x**5 - 15*x**4 + 10*x**3 if x <= 1 else 1)(s * d),
-    "Smoothstep 7": lambda s,d: (lambda x: 0 if x <= 0 else -20*x**7 + 70*x**6 - 84*x**5 + 35*x**4 if x <= 1 else 1)(s * d),
-    "Smoothstep 9": lambda s,d: (lambda x: 0 if x <= 0 else 70*x**9 - 315*x**8 + 540*x**7 - 420*x**6 + 126*x**5 if x <= 1 else 1)(s * d),
-
+    "Smoothstep 3": lambda s, d: (
+        lambda x: 0 if x <= 0 else 3 * x ** 2 - 2 * x ** 3 if x <= 1 else 1
+    )(s * d),
+    "Smoothstep 5": lambda s, d: (
+        lambda x: 0
+        if x <= 0
+        else 6 * x ** 5 - 15 * x ** 4 + 10 * x ** 3
+        if x <= 1
+        else 1
+    )(s * d),
+    "Smoothstep 7": lambda s, d: (
+        lambda x: 0
+        if x <= 0
+        else -20 * x ** 7 + 70 * x ** 6 - 84 * x ** 5 + 35 * x ** 4
+        if x <= 1
+        else 1
+    )(s * d),
+    "Smoothstep 9": lambda s, d: (
+        lambda x: 0
+        if x <= 0
+        else 70 * x ** 9 - 315 * x ** 8 + 540 * x ** 7 - 420 * x ** 6 + 126 * x ** 5
+        if x <= 1
+        else 1
+    )(s * d),
 }
 
-def apply_all_candidate_functions(actual_scores,test_weights):
-    return dict((description,minimize(function,actual_scores,test_weights).fun) for (description,function) in candidate_functions.items())
 
-def random_trial(students,tests,probability_of_writing_test):
-    actual_scores = np.random.uniform(0,1,(students,tests))
-    weights = np.random.uniform(0,1,tests)
-    not_written = np.random.uniform(0,1,(students,tests)) < (1 - probability_of_writing_test)
+def apply_all_candidate_functions(actual_scores, test_weights):
+    return dict(
+        (description, minimize(function, actual_scores, test_weights).fun)
+        for (description, function) in candidate_functions.items()
+    )
+
+
+def random_trial(students, tests, probability_of_writing_test):
+    actual_scores = np.random.uniform(0, 1, (students, tests))
+    weights = np.random.uniform(0, 1, tests)
+    not_written = np.random.uniform(0, 1, (students, tests)) < (
+        1 - probability_of_writing_test
+    )
     actual_scores[not_written] = np.nan
-    return apply_all_candidate_functions(actual_scores,weights)
+    return apply_all_candidate_functions(actual_scores, weights)
+
 
 raw_scores = """71.4,95.0,31.0,,,27.0,29.0,19.0,35.0,29.0
 81.0,100.0,,,,8.0,24.0,15.0,26.0,19.0
@@ -114,4 +160,6 @@ scores = raw_scores.split("\n")
 scores = [[float(x) if x else np.nan for x in line.split(",")] for line in scores]
 max_scores = np.array([100, 100, 50, 100, 50, 35, 35, 35, 35, 42])
 scores = np.array(scores) / max_scores
-weights = np.array([1/6, 1/15, 1/30, 1/30, 1/30, 1/9, 1/9, 1/9, 1/9, 2/9])
+weights = np.array(
+    [1 / 6, 1 / 15, 1 / 30, 1 / 30, 1 / 30, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 2 / 9]
+)
--- meta_experiment.py
+++ meta_experiment.py
@@ -1,50 +1,82 @@
 from experiment import *
 
+
 def piecewise_linear_prediction_function(xs, ys):
     def f(x):
-        for x1, x2, y1, y2 in zip([0] + list(xs), list(xs) + [1], [0] + list(ys), list(ys) + [1]):
+        for x1, x2, y1, y2 in zip(
+            [0] + list(xs), list(xs) + [1], [0] + list(ys), list(ys) + [1]
+        ):
             if x <= x2:
                 return y1 + (y2 - y1) / (x2 - x1) * (x - x1)
         return 1
+
     return lambda s, d: f(s * d)
 
+
 def piecewise_objective_function(actual_scores, test_weights, num_points, xs_and_ys):
-    return minimize(piecewise_linear_prediction_function(xs_and_ys[:num_points], xs_and_ys[num_points:]), actual_scores, test_weights).fun
+    return minimize(
+        piecewise_linear_prediction_function(
+            xs_and_ys[:num_points], xs_and_ys[num_points:]
+        ),
+        actual_scores,
+        test_weights,
+    ).fun
+
 
 def piecewise_initial_data(num_points):
-    equispaced = [(k + 1)/(num_points + 1) for k in range(num_points)]
+    equispaced = [(k + 1) / (num_points + 1) for k in range(num_points)]
     return equispaced + equispaced
+
 
 def piecewise_bounds(num_points):
     return [(0, 1)] * (2 * num_points)
 
+
 def piecewise_constraints(num_points):
-    return [{"type": "ineq", "fun": lambda xs_and_ys: xs_and_ys[0]},
-    *(
-        {"type": "ineq", "fun": lambda xs_and_ys: xs_and_ys[k] - xs_and_ys[k - 1]}
-        for k in range(1, num_points)
-    ),
-    {"type": "ineq", "fun": lambda xs_and_ys: 1 - xs_and_ys[num_points - 1]}]
+    return [
+        {"type": "ineq", "fun": lambda xs_and_ys: xs_and_ys[0]},
+        *(
+            {"type": "ineq", "fun": lambda xs_and_ys: xs_and_ys[k] - xs_and_ys[k - 1]}
+            for k in range(1, num_points)
+        ),
+        {"type": "ineq", "fun": lambda xs_and_ys: 1 - xs_and_ys[num_points - 1]},
+    ]
+
 
 def optimize_piecewise_prediction_function(num_points, actual_scores, test_weights):
-    return scipy.optimize.minimize(partial(piecewise_objective_function, actual_scores, test_weights, num_points),
-    piecewise_initial_data(num_points),
-    bounds=piecewise_bounds(num_points),
-    constraints=piecewise_constraints(num_points))
+    return scipy.optimize.minimize(
+        partial(piecewise_objective_function, actual_scores, test_weights, num_points),
+        piecewise_initial_data(num_points),
+        bounds=piecewise_bounds(num_points),
+        constraints=piecewise_constraints(num_points),
+    )
+
 
 def exponential_prediction_function(coefficients):
-    return lambda s, d: 1 - sum(c * np.exp(-(k + 1) * s * d) for (k, c) in enumerate(coefficients))
+    return lambda s, d: 1 - sum(
+        c * np.exp(-(k + 1) * s * d) for (k, c) in enumerate(coefficients)
+    )
+
 
 def exponential_initial_data(num_coefficients):
-    return [1/num_coefficients] * num_coefficients
+    return [1 / num_coefficients] * num_coefficients
+
 
 def exponential_contstraint(coefficients):
     return sum(coefficients) - 1
 
-def exponential_objective_function(actual_scores, test_weights, coefficients):
-    return minimize(exponential_prediction_function(coefficients), actual_scores, test_weights).fun
 
-def optimize_exponential_prediction_function(num_coefficients, actual_scores, test_weights):
-    return scipy.optimize.minimize(partial(exponential_objective_function, actual_scores, test_weights),
-    exponential_initial_data(num_coefficients),
-    constraints=[{"type": "eq", "fun": exponential_contstraint}])
+def exponential_objective_function(actual_scores, test_weights, coefficients):
+    return minimize(
+        exponential_prediction_function(coefficients), actual_scores, test_weights
+    ).fun
+
+
+def optimize_exponential_prediction_function(
+    num_coefficients, actual_scores, test_weights
+):
+    return scipy.optimize.minimize(
+        partial(exponential_objective_function, actual_scores, test_weights),
+        exponential_initial_data(num_coefficients),
+        constraints=[{"type": "eq", "fun": exponential_contstraint}],
+    )
