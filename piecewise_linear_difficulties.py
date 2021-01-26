3f6fd644bdec9b54ea93fb9e3f710e033c11281d
Dylan Nelson <nlsdyl001@myuct.ac.za>
2021-01-26 16:58:37 +0200

    update bounds and constraints according to new parameter order, and to hopefully be more efficient

--- piecewise_linear_difficulties.py
+++ piecewise_linear_difficulties.py
@@ -19,14 +19,12 @@ def badness_function(actual_scores, test_weights, pts_per_difficulty_fn):
     students = len(actual_scores)
     tests = len(test_weights)
 
-    def badness(allparams):
-        scores = allparams[:students]
-        mss = allparams[students: students +
-                        tests * pts_per_difficulty_fn].reshape(tests, pts_per_difficulty_fn)
-        dxss = allparams[students + tests *
-                        pts_per_difficulty_fn:].reshape(tests, pts_per_difficulty_fn)
+    def badness(all_params):
+        scores = all_params[:students]
+        mss = get_mss(students, tests, pts_per_difficulty_fn, all_params)
+        dxss = get_dxss(students, tests, pts_per_difficulty_fn, all_params)
         total = 0
-        for ms, dxs, w, a in zip(mss, dxss, test_weights, actual_scores):
+        for ms, dxs, w, a in zip(mss, dxss, test_weights, actual_scores.transpose()):
             d = difficulty_function(ms, dxs)
             total += np.nansum(w * (d(scores) - a)**2)
         return total
@@ -34,56 +32,52 @@ def badness_function(actual_scores, test_weights, pts_per_difficulty_fn):
     return badness
 
 
-def xcoord_constraint_fn(students, test_number, pts_per_difficulty_fn):
+def get_dxss(students, tests, pts_per_difficulty_fn, all_params):
+    return all_params[students + tests *
+                      pts_per_difficulty_fn:].reshape(tests, pts_per_difficulty_fn)
+
+
+def get_mss(students, tests, pts_per_difficulty_fn, all_params):
+    return all_params[students: students +
+                      tests * pts_per_difficulty_fn].reshape(tests, pts_per_difficulty_fn)
+
+
+def xcoord_constraint_fn(students, tests, pts_per_difficulty_fn):
     def constraint(all_params):
-        return (sum(
-            all_params[students +
-                       (2 * test_number + 1) * pts_per_difficulty_fn:students +
-                       (2 * test_number + 2) * pts_per_difficulty_fn]) - 1)
+        dxss = get_dxss(students, tests, pts_per_difficulty_fn, all_params)
+        return np.all(np.sum(dxss, 1) == 1) - 1
 
     return constraint
 
 
-def ycoord_constraint_fn(students, test_number, pts_per_difficulty_fn):
+def ycoord_constraint_fn(students, tests, pts_per_difficulty_fn):
     def constraint(all_params):
-        ms = all_params[students +
-                        2 * test_number * pts_per_difficulty_fn:students +
-                        (2 * test_number + 1) * pts_per_difficulty_fn]
-        dxs = all_params[students + (2 * test_number + 1) *
-                        pts_per_difficulty_fn:students +
-                        (2 * test_number + 2) * pts_per_difficulty_fn]
-        return np.array(ms).dot(np.array(dxs)) - 1
+        mss = get_mss(students, tests, pts_per_difficulty_fn, all_params)
+        dxss = get_dxss(students, tests, pts_per_difficulty_fn, all_params)
+        return np.all(np.sum(mss * dxss, 1) == 1) - 1
 
     return constraint
 
 
 def constraints(students, tests, pts_per_difficulty_fn):
     return [
-        *({
+        {
             "type": "eq",
-            "fun": xcoord_constraint_fn(students, k, pts_per_difficulty_fn),
-        } for k in range(tests)),
-        *({
+            "fun": xcoord_constraint_fn(students, tests, pts_per_difficulty_fn),
+        },
+        {
             "type": "eq",
-            "fun": ycoord_constraint_fn(students, k, pts_per_difficulty_fn),
-        } for k in range(tests)),
+            "fun": ycoord_constraint_fn(students, tests, pts_per_difficulty_fn),
+        }
     ]
 
 
 def bounds(students, tests, pts_per_difficulty_fn):
-    return [(0, 1)] * students + list(
-        chain(*([[(0, None)] * pts_per_difficulty_fn +
-                 [(0, 1)] * pts_per_difficulty_fn] * tests)))
-
-
-def initial_values_per_test(pts_per_difficulty_fn):
-    return [1] * pts_per_difficulty_fn + [1 / pts_per_difficulty_fn
-                                          ] * pts_per_difficulty_fn
+    return [(0, 1)] * students + [(0, None)] * (pts_per_difficulty_fn * tests) + [(0, 1)] * (pts_per_difficulty_fn * tests)
 
 
 def initial_value(students, tests, pts_per_difficulty_fn):
-    return [0.5] * students + list(
-        chain(*([initial_values_per_test(pts_per_difficulty_fn)] * tests)))
+    return np.append(np.ones(students) / 2, np.ones(pts_per_difficulty_fn * tests), np.ones(pts_per_difficulty_fn * tests) / pts_per_difficulty_fn)
 
 
 def minimize(actual_scores, test_weights, pts_per_difficulty_fn, **kwargs):
