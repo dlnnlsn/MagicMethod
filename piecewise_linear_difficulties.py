43990ef5e0ba2cd7ce0038d7d44db87e7cd72e86
Dylan Nelson <nlsdyl001@myuct.ac.za>
2021-01-26 16:35:05 +0200

    rename bs to dxs to better reflect its meaning

--- piecewise_linear_difficulties.py
+++ piecewise_linear_difficulties.py
@@ -4,14 +4,14 @@ import scipy.optimize
 from itertools import chain, starmap
 
 
-def difficulty_function(ms, bs):
-    ys = ms * bs
+def difficulty_function(ms, dxs):
+    ys = ms * dxs
     ys = ys.cumsum() - ys
-    bs = bs.cumsum() - bs
+    xs = dxs.cumsum() - dxs
 
     def d(s):
-        index = np.searchsorted(bs, s) - 1
-        return (x > 0) * (ys[index] + ms[index] * (s - bs[index]))
+        index = np.searchsorted(xs, s) - 1
+        return (s > 0) * (ys[index] + ms[index] * (s - xs[index]))
     return d
 
 
@@ -23,11 +23,11 @@ def badness_function(actual_scores, test_weights, pts_per_difficulty_fn):
         scores = allparams[:students]
         mss = allparams[students: students +
                         tests * pts_per_difficulty_fn].reshape(tests, pts_per_difficulty_fn)
-        bss = allparams[students + tests *
+        dxss = allparams[students + tests *
                         pts_per_difficulty_fn:].reshape(tests, pts_per_difficulty_fn)
         total = 0
-        for ms, bs, w, a in zip(mss, bss, test_weights, actual_scores):
-            d = difficulty_function(ms, bs)
+        for ms, dxs, w, a in zip(mss, dxss, test_weights, actual_scores):
+            d = difficulty_function(ms, dxs)
             total += np.nansum(w * (d(scores) - a)**2)
         return total
 
@@ -49,10 +49,10 @@ def ycoord_constraint_fn(students, test_number, pts_per_difficulty_fn):
         ms = all_params[students +
                         2 * test_number * pts_per_difficulty_fn:students +
                         (2 * test_number + 1) * pts_per_difficulty_fn]
-        bs = all_params[students + (2 * test_number + 1) *
+        dxs = all_params[students + (2 * test_number + 1) *
                         pts_per_difficulty_fn:students +
                         (2 * test_number + 2) * pts_per_difficulty_fn]
-        return np.array(ms).dot(np.array(bs)) - 1
+        return np.array(ms).dot(np.array(dxs)) - 1
 
     return constraint
 
